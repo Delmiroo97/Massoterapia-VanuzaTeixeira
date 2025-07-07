0da7c81e4cd039519c82662e1cc471d7d8e7352a
Fernando Delmiro <119796795+Delmiroo97@users.noreply.github.com>
2025-07-07 11:13:08 -0300

    menu-mobile criado e estilizado, mas ainda em fase de testes, pode sofrer alterações a qualquer momento.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -28,3 +28,10 @@
              }
         },
     });
+
+    const btnMenu = document.getElementById('js-btn-menu');
+
+    btnMenu.addEventListener('click', () => {
+        document.documentElement.classList.toggle('opened-menu');
+        btnMenu.classList.toggle('is-active')
+    });
